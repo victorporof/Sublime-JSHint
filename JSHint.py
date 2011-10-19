51fa230c64f9fbd03cedc46f0e09977437703b51
Victor Porof <victor.porof@gmail.com>
2011-10-19 16:44:05 +0300

    Updated default lint settings

--- JSHint.py
+++ JSHint.py
@@ -4,13 +4,16 @@ import sublime, sublime_plugin
 class JshintCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
-    regx = re.compile(" ");
+    regx = re.compile(" ")
     lint = commands.getoutput("node " +
       regx.sub("\ ", sublime.packages_path()) + "/JSHint/scripts/run.js " +
       regx.sub("\ ", self.view.file_name()) +
-        " es5:\ true" +
         " browser:\ true" +
-        " globalstrict:\ true")
+        " es5:\ true" +
+        " v8:\ true" +
+        " trailing:\ true" +
+        " onevar:\ true" +
+        " sub:\ true")
 
     if len(lint) > 0:
       print lint
