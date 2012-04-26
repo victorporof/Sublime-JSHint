fe7405ef1f6dd4855217f46ba489c26e1cf4cb9e
Victor Porof <victor.porof@gmail.com>
2012-04-26 16:31:11 -0400

    Simplified the command run method

--- JSHint.py
+++ JSHint.py
@@ -1,23 +1,25 @@
-import commands, re
+import commands
 import sublime, sublime_plugin
 
 class JshintCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
-    regx = re.compile(" ")
-    lint = commands.getoutput("node " +
-      regx.sub("\ ", sublime.packages_path()) + "/Sublime-JSHint/scripts/run.js " +
-      regx.sub("\ ", self.view.file_name()) +
-        " browser:\ true" +
-        " v8:\ true" +
-        " es5:\ true" +
-        " esnext:\ true" +
-        " globalstrict:\ true" +
-        " strictundef:\ true" +
-        " laxradix:\ true" +
-        " trailing:\ true" +
-        " undef:\ false" +
-        " sub:\ true")
+    scriptPath = sublime.packages_path() + "/Sublime-JSHint/scripts/run.js"
+    setings = ' '.join([
+      "browser:\ true",
+      "v8:\ true",
+      "es5:\ true",
+      "esnext:\ true",
+      "globalstrict:\ true",
+      "strictundef:\ true",
+      "laxradix:\ true",
+      "trailing:\ true",
+      "undef:\ false",
+      "sub:\ true"
+    ])
+    cmd = ["node",scriptPath,self.view.file_name(),setings]
+
+    lint = commands.getoutput('"'+'" "'.join(cmd)+'"')
 
     if len(lint) > 0:
       print lint
