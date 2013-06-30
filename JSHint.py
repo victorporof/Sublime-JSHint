770d5abc0a438f1fb5886fc3b4c92d9af87a9638
Victor Porof <vporof@mozilla.com>
2013-06-30 23:10:14 +0300

    Better output for jshint errors

--- JSHint.py
+++ JSHint.py
@@ -5,21 +5,48 @@ class JshintCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
     scriptPath = sublime.packages_path() + "/Sublime-JSHint/scripts/run.js"
-    setings = ' '.join([
-      "browser:\ true",
-      "v8:\ true",
-      "es5:\ true",
-      "esnext:\ true",
-      "globalstrict:\ true",
-      "strictundef:\ true",
-      "laxradix:\ true",
-      "trailing:\ true",
-      "undef:\ false",
-      "sub:\ true"
+    filePath = self.view.file_name()
+    setings = ','.join([
+      "browser: true",
+      "node: true",
+      "esnext: true",
+      "moz: true",
+      "globalstrict: true",
+      "trailing: true",
+      "undef: true",
+      "unused: true"
     ])
-    cmd = ["node",scriptPath,self.view.file_name(),setings]
 
-    lint = commands.getoutput('"'+'" "'.join(cmd)+'"')
+    cmd = ["node", scriptPath, filePath, setings]
+    lint = commands.getoutput('"' + '" "'.join(cmd) + '"')
+
+    self.view.erase_regions("jshint_errors");
 
     if len(lint) > 0:
+      regions = []
+      menuitems = []
+
+      for line in sorted(lint.splitlines()):
+        try:
+          data = line.split(":")
+          line = int(data[1]) - 1
+          column = int(data[2])
+          point = self.view.text_point(line, column)
+          menuitems.append(data[1] + ":" + data[2] + " " + data[3])
+          regions.append(sublime.Region(point - 1, point))
+        except:
+          pass
+
+      self.view.add_regions("jshint_errors", regions, " ", "cross")
+      sublime.active_window().show_quick_panel(menuitems, self.on_chosen)
       print lint
+
+  def on_chosen(self, index):
+    if index == -1:
+      return
+
+    region = self.view.get_regions("jshint_errors")[index]
+    selection = self.view.sel()
+    selection.clear()
+    selection.add(region)
+    self.view.show(region)
--- scripts/run.js
+++ scripts/run.js
@@ -21,102 +21,101 @@
  *    distribution.
  */
 
-/*global console, process, require, __dirname */
-
 (function() {
   "use strict";
 
-  var i, len, hash, key, value, raw,
+  // Cache the console log function and the process arguments.
+  var log = console.log;
+  var argv = process.argv;
 
-    // cache the console log function and the process arguments
-    log = console.log,
-    argv = process.argv,
+  // Require path and file system utilities to load the jshint.js file.
+  var path = require('path');
+  var fs = require('fs');
+  var jshint = require("jshint/src/stable/jshint.js").JSHINT;
 
-    // require path and file system utilities to load the jshint.js file
-    path = require('path'),
-    fs = require('fs'),
-    jshint = require(path.join(__dirname, "jshint.js")).JSHINT,
+  // The source file to be linted and options.
+  var source = argv[2] || "";
+  var option = {};
 
-    // the source file to be linted and options
-    source = argv[2] || "",
-    option = {};
+  // Continue only if the source file is specified.
+  if (source == "") {
+    return;
+  }
 
-  // continue only if the source file is specified
-  if (source !== "") {
-    // when a JSHint config file exists in the same dir as
-    // the source file or one dir above, then use this
-    // configuration instead of the default one
-    var filename = ".jshintrc",
-        getOptions = function (file) {
-          var data = fs.readFileSync(file, "utf8");
-          return JSON.parse(data);
-        };
+  // When a JSHint config file exists in the same dir as the source file or
+  // one dir above, then use this configuration instead of the default one.
+  var jshintrc = ".jshintrc";
+  var getOptions = function(file) {
+    var data = fs.readFileSync(file, "utf8");
+    return JSON.parse(data);
+  };
 
-    // extra arguments with custom options could be passed, so check them now
-    // and add them to the options object
-    for (i = 3, len = argv.length; i < len; i++) {
-      hash = argv[i].split(":");
-      key = hash[0];
-      value = hash[1];
+  if (fs.existsSync(jshintrc)) {
+    option = getOptions(jshintrc);
+  } else if (fs.existsSync("../" + jshintrc)) {
+    option = getOptions("../" + jshintrc);
+  } else {
+    // Extra arguments with custom options could be passed, so check them now
+    // and add them to the options object.
+    var settings = argv[3].split(",");
+    for (var i = 0, len = settings.length; i < len; i++) {
+      var hash = settings[i].split(":");
+      var key = hash[0].trim();
+      var value = hash[1].trim();
 
-      // there are two options that allow numerical values
-      if (key === "maxerr" || key === "indent") {
-        option[key] = +value; //store value as Number, not as String
+      log(key + " " + value + "\n\n");
+
+      // There are two options that allow numerical values.
+      if (key == "maxerr" || key == "indent") {
+        // Store value as Number, not as String.
+        option[key] = +value;
         continue;
       }
 
-      // there is one option that allows array of strings to be passed (predefined custom globals)
-      if (key === "predef") {
-        option[key] = eval(value);//eval is evil, but JSON.parse would require only double quotes to be used
+      // There is one option that allows array of strings to be passed
+      // (predefined custom globals)
+      if (key == "predef") {
+        // eval is evil, but JSON.parse would require usage of only double quotes
+        option[key] = eval(value);
         continue;
       }
 
-      // options are stored in key value pairs, such as option.es5 = true
-      option[key] = value === true || value.trim() === "true";
+      // Options are stored in key value pairs, such as option.es5 = true
+      option[key] = value == "true";
+    }
+  }
+
+  // Read the source file and, when complete, lint the code.
+  fs.readFile(source, "utf8", function(err, data) {
+    if (err) {
+      log("Error, unable to continue.");
+      return;
     }
 
-    // when a JSHint config file exists in the same dir as
-    // the source file or one dir above, then use this
-    // configuration instead of the default one
-    if (fs.existsSync(filename)) {
-      option = getOptions(filename);
-    } else if (fs.existsSync("../" + filename)) {
-      option = getOptions("../" + filename);
-    }
+    // Lint the code and write readable error output to the console.
+    try {
+      jshint(data, option);
+    } catch(e) {}
 
-    // read the source file and, when complete, lint the code
-    fs.readFile(source, "utf8", function(err, data) {
-      if (err) {
+    jshint.errors.forEach(function(e) {
+      // If the argument is null, then we could not continue (too many errors).
+      if (!e) {
+        log("Stopping, unable to continue.");
         return;
       }
 
-      // lint the code and write readable error output to the console
-      try {
-        jshint(data, option);
-      } catch(e) {}
-
-      jshint.errors.forEach(function(e) {
-
-        // if the error is null, then we could not continue (too many errors)
-        if (e === null) {
-          log("Stopping, unable to continue.");
-          return;
-        }
-
-        // get the raw error data
-        raw = e.raw;
-
-        // do some formatting if the error data is available
-        if ("undefined" !== typeof raw) {
-          log([source, ":",
-               e.line, ":",
-               e.character, " ",
-               raw.replace("{a}", e.a).
-                   replace("{b}", e.b).
-                   replace("{c}", e.c).
-                   replace("{d}", e.d)].join(""));
-        }
-      });
+      // Do some formatting if the error data is available.
+      if (e.raw) {
+        log([
+          source.split(path.sep).pop(), ":",
+          e.line, ":",
+          e.character, ":",
+          e.raw.replace("{a}", e.a)
+               .replace("{b}", e.b)
+               .replace("{c}", e.c)
+               .replace("{d}", e.d)
+        ].join(""));
+      }
     });
-  }
+  });
 }());
